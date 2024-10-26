ebd16627c482dc608291f43109c65ca4e2385d8e
amiraroumili <131705940+amiraroumili@users.noreply.github.com>
2024-10-26 11:11:59 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,29 +2,31 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/add/<int:num1>/<int:num2>', methods=['GET'])
+@app.route('/api/add/<int:num1>/<int:num2>', methods=['GET'])
 def add(num1, num2):
     result = num1 + num2
     return jsonify({'status': 200, 'result': result})
 
-@app.route('/subtract/<int:num1>/<int:num2>', methods=['GET'])
+@app.route('/api/subtract/<int:num1>/<int:num2>', methods=['GET'])
 def subtract(num1, num2):
     result = num1 - num2
     return jsonify({'status': 200, 'result': result})
 
-@app.route('/multiply/<int:num1>/<int:num2>', methods=['GET'])
+@app.route('/api/multiply/<int:num1>/<int:num2>', methods=['GET'])
 def multiply(num1, num2):
     result = num1 * num2
     return jsonify({'status': 200, 'result': result})
 
-@app.route('/divide/<int:num1>/<int:num2>', methods=['GET'])
+@app.route('/api/divide/<int:num1>/<int:num2>', methods=['GET'])
 def divide(num1, num2):
     if num2 == 0:
         return jsonify({'status': 400, 'error': 'Division by zero is not allowed'})
     result = num1 / num2
     return jsonify({'status': 200, 'result': result})
 
+# Ensure the app runs properly on Vercel
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
 
 
